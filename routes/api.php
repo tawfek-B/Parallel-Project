6d4c78287d420006ad9885496d34c90751a9a8ce
anisa <anisa@gmail.com>
2026-05-12 04:41:17 +0300

    api update

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,21 @@
 <?php
-
+use App\Http\Controllers\OrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Jobs\GenerateInvoiceJob;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::post('orders', [\App\Http\Controllers\OrderController::class, 'store']);
+
+// العمل بالخلفية للمهام الغير اساسية
+
+Route::get('/invoice-without', [OrderController::class, 'createOrderWithInvoiceSync']);
+Route::get('/invoice-with', [OrderController::class, 'createOrderWithInvoiceAsync']);
+
+Route::get('/daily-sales-batch', [OrderController::class, 'dailySalesBatch']);
+Route::get('/before', [OrderController::class, 'before']);
+Route::get('/after', [OrderController::class, 'after']);
